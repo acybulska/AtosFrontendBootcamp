1e1645e5150cfa07ff9d40f1317cf4793b99dcfd
Lisa <aleksandralisacybulska@gmail.com>
2018-08-20 12:21:33 +0200

    Finished ex4

--- section2/ex4.js
+++ section2/ex4.js
@@ -1,11 +1,37 @@
 new Vue({
   el: '#exercise',
   data: {
-
+    effectClasses: {
+      highlight: false,
+      shrink: true
+    },
+    newClass: '',
+    checkClass: true,
+    newStyle: {
+      width: '200px',
+      height: '200px',
+      backgroundColor: 'black'
+    },
+    progressBar: {
+      width: '0px',
+      backgroundColor: 'yellow'
+    }
   },
   methods: {
-    startEffect: function() {
-    
+    startEffect: function () {
+      var vm = this;
+      setInterval(function () {
+        vm.effectClasses.highlight = !vm.effectClasses.highlight;
+        vm.effectClasses.shrink = !vm.effectClasses.shrink;
+      }, 1000);
+    },
+    startProgress: function () {
+      var vm = this;
+      var width = 0;
+      setInterval(function () {
+        width = width + 10;
+        vm.progressBar.width = width + 'px';
+      }, 500)
     }
   }
 });
