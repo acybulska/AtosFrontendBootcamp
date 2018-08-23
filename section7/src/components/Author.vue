8b14a1363f8844e4ad1d8e137977942104e0193d
Lisa <aleksandralisacybulska@gmail.com>
2018-08-23 11:40:01 +0200

    Quotes project part 1

---
+++ section7/src/components/Author.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h3>New Quote</h3>        
+    </div>
+</template>
+<script>
+    export default {
+        props: ['quote']
+    };
+</script>
+<style scoped>
+</style>
---
+++ section7/src/components/Quote.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div class="title">
+         <slot name="title"></slot>
+         <span><slot name="subtitle">subtitle</slot></span>
+        </div>
+        <hr>
+        <div>
+         <slot name="content"></slot>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        props: ['quote']
+    };
+</script>
+<style scoped>
+    div {
+        border: 1px solid #ccc;
+        box-shadow: 1px 1px 2px black;
+        padding: 30px;
+        margin: 30px auto;
+        text-align: center;
+    }
+    h2 {
+        color: red;
+    }
+</style>
