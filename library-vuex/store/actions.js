ef44ee17efcd460fbcf3d39dc3f6bd6bb3b259dd
Lisa <aleksandralisacybulska@gmail.com>
2018-09-25 15:14:19 +0200

    Finished add book

--- library-vuex/store/actions.js
+++ library-vuex/store/actions.js
@@ -1,23 +1,27 @@
-import {loadBooks} from "../api/books";
+import { loadBooks } from "../api/books";
 import axios from 'axios'
 
 export default {
-  markBook({commit}, payload){
+  markBook({ commit }, payload) {
     commit('markBook', payload.id)
   },
-    deleteBook({commit}, payload){
+  deleteBook({ commit }, payload) {
     commit('deleteBook', payload.id)
   },
-    addBook({commit}, payload){
-      axios.post('http://bootcamp.opole.pl/books/add-book/mx5t', {
-      description: payload.book.description,
-      title: payload.book.title
+  async addBook({ commit }, payload) {
+    const params = new URLSearchParams();
+    params.append('title', payload.book.title);
+    params.append('description', payload.book.description);
+    await axios({
+      method: 'post',
+      url: 'http://bootcamp.opole.pl/books/add-book/mx5t',
+      data: params
     }).then(function (response) {
       console.log(response.data)
       commit('addBook', payload.book)
     })
   },
-  async initBooks({commit}){
+  async initBooks({ commit }) {
     commit('initBooks', await loadBooks())
   }
 }
