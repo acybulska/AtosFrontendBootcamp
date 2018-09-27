479cf68fa051f3d7a2a47ce4cfa0be8bc255678c
Lisa <aleksandralisacybulska@gmail.com>
2018-09-27 15:21:08 +0200

    Delete multiple books

--- library-vuex/components/BookFilter.vue
+++ library-vuex/components/BookFilter.vue
@@ -1,22 +1,33 @@
 <template>
  <b-button-group>
-     <b-button @click="getList()">Get list</b-button>
+     <b-button variant="success" @click="addMultiple()">Add multiple</b-button>
+     <b-button variant="danger" @click="deleteMultiple()">Delete multiple</b-button>
  </b-button-group>
 </template>
 <script>
-import bButtonGroup from 'bootstrap-vue/es/components/button-group/button-group';
+import bButtonGroup from "bootstrap-vue/es/components/button-group/button-group";
 
 export default {
-    computed: {
-        multipleID() {
-            return this.$store.getters.multipleID ? this.$store.getters.multipleID : [];
-        }
-    },
-    methods: {
-        getList() {
-            console.log(this.multipleID)
-        }
+  computed: {
+    multipleID() {
+      return this.$store.getters.multipleID
+        ? this.$store.getters.multipleID
+        : [];
     }
-}
+  },
+  methods: {
+    addMultiple() {
+      // for (const i of val) {
+      //     this.$store.dispatch("addBook", {book: book})
+      //     }
+    },
+    deleteMultiple() {
+      console.log(this.multipleID);
+      for (const i of this.multipleID) {
+        this.$store.dispatch("deleteBook", { id: i });
+      }
+    }
+  }
+};
 </script>
 <style></style>
